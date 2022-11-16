a79e0b4dcca2848ef99622606e520c94e0dfea15
kaivladimirv <kaivladimirv@gmail.com>
2022-11-16 06:11:15 +0600

    tests: Add a test that checks the correctness of comparing flat json files

---
+++ tests/DiffJsonTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Differ\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use function Differ\Differ\genDiff;
+
+class DiffJsonTest extends TestCase
+{
+    protected string $pathToFile1;
+    protected string $pathToFile2;
+
+    protected function setUp(): void
+    {
+        $this->pathToFile1 = $this->getFixtureFullPath('fileFlat1.json');
+        $this->pathToFile2 = $this->getFixtureFullPath('fileFlat2.json');
+    }
+
+    private function getFixtureFullPath($fixtureName): string
+    {
+        $parts = [
+            __DIR__,
+            'fixtures',
+            $fixtureName,
+        ];
+
+        return implode('/', $parts);
+    }
+
+    public function testFlatJson(): void
+    {
+        $expected = '{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}';
+
+        $this->assertEquals($expected, genDiff($this->pathToFile1, $this->pathToFile2));
+    }
+}
